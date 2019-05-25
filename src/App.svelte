1456a561850bf3d64e45833c572b7d5f05d25ed8
Imre Bartis <imre.bartis@siili.com>
2019-05-25 04:58:46 +0300

    use reactive variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,15 @@
 <script>
 	let name = 'Mix';
 	let page = 1;
+
+	$: uppercaseName = name.toUpperCase();
 	
 	function incrementPage() {
 		page += 1
+	}
+
+	function changeName() {
+		name = 'Fix';
 	}
 </script>
 
@@ -13,5 +19,6 @@
 	}
 </style>
 
-<h1>Hello {name.toUpperCase()}, my page is {page}!</h1>
-<button on:click="{incrementPage}">Change page</button>
+<h1>Hello {uppercaseName}, my page is {page}!</h1>
+<button on:click='{incrementPage}'>Change page</button>
+<button on:click='{changeName}'>Change name</button>
