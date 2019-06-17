c79c206e84f750e1cae989ee6b9463f3b52c492a
Imre Bartis <imre.bartis@siili.com>
2019-06-17 12:23:21 +0300

    add more bindings

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,49 @@
 <script>
-	import ContactCard from './ContactCard.svelte';
+  import ContactCard from "./ContactCard.svelte";
 
-	let name = 'Mix';
-	let page = 1;
+  let name = "Mix";
+  let title = "";
+  let image = "";
+  let description = "";
+  let page = 1;
 
-	$: uppercaseName = name.toUpperCase();
-	$: console.log(name);
-	$: if(name === 'Fix') {
-		console.log('it works');
-		page = 1000;
-	}
-	
-	function incrementPage() {
-		page += 1;
-	}
+  $: uppercaseName = name.toUpperCase();
+  $: console.log(name);
+  $: if (name === "Fix") {
+    console.log("it works");
+    page = 1000;
+  }
 
-	function changeName() {
-		name = 'Fix';
-	}
+  function incrementPage() {
+    page += 1;
+  }
 
-	function nameInput(event) {
-		const enteredValue = event.target.value;
-		name = enteredValue;
-	}
+  function changeName() {
+    name = "Fix";
+  }
+
+  function nameInput(event) {
+    const enteredValue = event.target.value;
+    name = enteredValue;
+  }
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: purple;
+  }
 </style>
 
 <h1>Hello {uppercaseName}, my page is {page}!</h1>
-<button on:click='{incrementPage}'>Change page</button>
+<button on:click={incrementPage}>Change page</button>
 <!-- <button on:click='{changeName}'>Change name</button> -->
 <!-- <input type="text" value="{name}" on:input="{nameInput}"> -->
-<input type="text" value="{name}" bind:value="{name}">
-<ContactCard userName="{name}" />
+<input type="text" bind:value={name} />
+<input type="text" bind:value={title} />
+<input type="text" bind:value={image} />
+<textarea rows="3" bind:value={description} />
+<ContactCard 
+	userName={name}
+	jobTitle={title}
+	{description}
+	userImage={image} />
