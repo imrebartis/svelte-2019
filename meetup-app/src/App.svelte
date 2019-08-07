9edf9db10703022d306234f5af36a6aa0f597eda
Imre Bartis <imre.bartis@siili.com>
2019-08-07 07:04:24 +0300

    parse data into components

--- meetup-app/src/App.svelte
+++ meetup-app/src/App.svelte
@@ -1,27 +1,28 @@
 <script>
-  import Header from './UI/Header.svelte';
-  import MeetupItem from './Meetups/MeetupItem.svelte';
+  import Header from "./UI/Header.svelte";
+  import MeetupItem from "./Meetups/MeetupItem.svelte";
 
   const meetups = [
-      {
-        id: 'm1',
-        title: 'Coding Bootcamp',
-        subtitle: 'learn to code in 1 sec',
-        description: 'Organic XOXO church-key kitsch neutra subway tile cray meggings edison bulb vegan.',
-        imageUrl: 'https://picsum.photos/200/300',
-        address: '27th Nördern Road, 23323, Nörd Yörk',
-        contactEmail: 'code@test.com'
-      },
-      {
-        id: 'm1',
-        title: 'Chill 4ever',
-        subtitle: 'learn how to chill in 1 sec',
-        description: 'Jianbing tumblr tattooed, yr vinyl microdosing franzen synth cray cred cardigan pug. Literally vaporware art party, blue bottle listicle ethical single-origin coffee aesthetic live-edge fingerstache.',
-        imageUrl: 'https://picsum.photos/200/300',
-        address: '27th Chilly Road, 23323, Chill Fork',
-        contactEmail: 'chill@test.com'
-      }
-
+    {
+      id: "m1",
+      title: "Coding Bootcamp",
+      subtitle: "learn to code in 1 sec",
+      description:
+        "Organic XOXO church-key kitsch neutra subway tile cray meggings edison bulb vegan.",
+      imageUrl: "https://picsum.photos/200/300",
+      address: "27th Nördern Road, 23323, Nörd Yörk",
+      contactEmail: "code@test.com"
+    },
+    {
+      id: "m1",
+      title: "Chill 4ever",
+      subtitle: "learn how to chill in 1 sec",
+      description:
+        "Jianbing tumblr tattooed, yr vinyl microdosing franzen synth cray cred cardigan pug. Literally vaporware art party, blue bottle listicle ethical single-origin coffee aesthetic live-edge fingerstache.",
+      imageUrl: "https://picsum.photos/200/300",
+      address: "27th Chilly Road, 23323, Chill Fork",
+      contactEmail: "chill@test.com"
+    }
   ];
 </script>
 
@@ -33,8 +34,14 @@
 
 <Header />
 
-<section id="meetups"></section>
+<section id="meetups" />
 
 {#each meetups as meetup}
-  <MeetupItem />
+  <MeetupItem
+    title={meetup.title}
+    subtitle={meetup.subtitle}
+    imageUrl={meetup.imageUrl}
+    description={meetup.description}
+    email={meetup.contactEmail}
+    address={meetup.address} />
 {/each}
