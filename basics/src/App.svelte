a04b3409920a9ef7119949155da0aaaa45ee969c
Imre Bartis <imre.bartis@siili.com>
2020-02-04 16:13:12 +0200

    revisit two-way binding

--- basics/src/App.svelte
+++ basics/src/App.svelte
@@ -1,49 +1,12 @@
 <script>
-  import ContactCard from "./ContactCard.svelte";
+  let val = "boh";
 
-  let name = "Mix";
-  let title = "";
-  let image = "";
-  let description = "";
-  let page = 1;
+  $: console.log(val);
 
-  $: uppercaseName = name.toUpperCase();
-  $: console.log(name);
-  $: if (name === "Fix") {
-    console.log("it works");
-    page = 1000;
-  }
-
-  function incrementPage() {
-    page += 1;
-  }
-
-  function changeName() {
-    name = "Fix";
-  }
-
-  function nameInput(event) {
-    const enteredValue = event.target.value;
-    name = enteredValue;
+  function setValue(event) {
+    val = event.target.value;
   }
 </script>
 
-<style>
-  h1 {
-    color: purple;
-  }
-</style>
-
-<h1>Hello {uppercaseName}, my page is {page}!</h1>
-<button on:click={incrementPage}>Change page</button>
-<!-- <button on:click='{changeName}'>Change name</button> -->
-<!-- <input type="text" value="{name}" on:input="{nameInput}"> -->
-<input type="text" bind:value={name} />
-<input type="text" bind:value={title} />
-<input type="text" bind:value={image} />
-<textarea rows="3" bind:value={description} />
-<ContactCard 
-	userName={name}
-	jobTitle={title}
-	{description}
-	userImage={image} />
+<!-- <input type="text" value={val} on:input={setValue}> -->
+<input type="text" bind:value={val}>
