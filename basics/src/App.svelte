771e876d0df020cd4dba226019b0a2527a72c44e
Imre Bartis <imre.bartis@siili.com>
2020-02-13 16:05:47 +0200

    bind <select> dropdown

--- basics/src/App.svelte
+++ basics/src/App.svelte
@@ -5,10 +5,18 @@
   let val = "boh";
   let price = "0";
   let selectedOption = 1;
+  let agreed;
+  let favColor = "bloo";
+  let favCat = ["tiger"];
+  let singleFavColor = "blue";
 
   $: console.log(val);
   $: console.log(selectedOption);
   $: console.log(price);
+  $: console.log(agreed);
+  $: console.log(favColor);
+  $: console.log(favCat);
+  $: console.log(singleFavColor);
 
   function setValue(event) {
     val = event.target.value;
@@ -21,3 +29,42 @@
 <Toggle bind:chosenOption={selectedOption} />
 
 <input type="number" bind:value={price} />
+
+<label>
+  <input type="checkbox" bind:checked={agreed} />
+  Agree to terms
+</label>
+
+<h1>Favorite color</h1>
+<label>
+  <input type="radio" value="yello" name="color" bind:group={favColor} />
+  Yello
+</label>
+<label>
+  <input type="radio" value="bloo" name="color" bind:group={favColor} />
+  Bloo
+</label>
+<label>
+  <input type="radio" value="black" name="color" bind:group={favColor} />
+  Black
+</label>
+
+<h1>Favorite cat</h1>
+<label>
+  <input type="checkbox" value="cheshire" name="color" bind:group={favCat} />
+  Cheshire
+</label>
+<label>
+  <input type="checkbox" value="tiger" name="color" bind:group={favCat} />
+  Tiger
+</label>
+<label>
+  <input type="checkbox" value="puma" name="color" bind:group={favCat} />
+  Puma
+</label>
+
+<select bind:value={singleFavColor}>
+  <option value="green">Green</option>
+  <option value="red">Red</option>
+  <option value="blue">Blue</option>
+</select>
