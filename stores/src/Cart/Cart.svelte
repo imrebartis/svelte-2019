11b6d987dcc75ad98d81719ca39e047bbd4259ac
Imre Bartis <imre.bartis@siili.com>
2020-03-30 15:28:06 +0300

    create a writable store & subscribe

---
+++ stores/src/Cart/Cart.svelte
@@ -0,0 +1,37 @@
+<script>
+  import CartItems from "./cart-store.js";
+  import CartItem from "./CartItem.svelte";
+
+  let items;
+
+  CartItems.subscribe(its => {
+    items = its;
+  });
+
+</script>
+
+<style>
+  section {
+    width: 30rem;
+    max-width: 90%;
+    margin: 2rem auto;
+    border-bottom: 2px solid #ccc;
+  }
+
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+</style>
+
+<section>
+  <h1>Cart</h1>
+  <ul>
+    {#each items as item (item.id)}
+      <CartItem id={item.id} title={item.title} price={item.price} />
+    {:else}
+      <p>No items in cart yet!</p>
+    {/each}
+  </ul>
+</section>
